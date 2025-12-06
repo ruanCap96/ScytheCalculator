6919761115c6707ef416469b3112c519628e6d2b
Ruan Hart <ruan.hart@capgemini.com>
2025-12-06 16:21:45 +0100

    canShowWinningFaction input added

--- src/app/player-faction/shell/shell.component.ts
+++ src/app/player-faction/shell/shell.component.ts
@@ -4,11 +4,11 @@ import { PlayerFactionModel } from '../models/player-faction.model';
 @Component({
   selector: 'player-faction-shell',
   templateUrl: './shell.component.html',
-  styleUrls: ['./shell.component.scss']
+  styleUrls: ['./shell.component.scss'],
 })
-
 export class ShellComponent {
   @Input() playerFactions: PlayerFactionModel[];
   @Input() winningPlayerFaction: PlayerFactionModel;
+  @Input() canShowWinningFaction: boolean;
   @Input() error: string;
 }
